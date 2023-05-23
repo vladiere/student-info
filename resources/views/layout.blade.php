34263d19c4c92ff4e907cd681f8b2de9aad8a660
vladiere <ruthakaza@gmail.com>
2023-05-23 12:38:36 +0800

    committed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -44,6 +44,14 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="{{ url('/students') }}">Add Student</a>
                         </li>
+                        <li class="nav-item">
+                            <form action="/search" method="GET"
+                                class="form-inline mt-2 mt-md-0 d-flex flex-row gap-4">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search"
+                                    aria-label="Search" name="query">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                        </li>
                     </ul>
                 </div>
             </div>
